3d1e763742d45dc3d6df040d083214a979f5ce96
Charpell <charpellumeh@gmail.com>
2019-03-10 00:32:10 +0100

    Write tests

--- client/src/App.js
+++ client/src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
   }
 
   callApi = async () => {
-    const response = await fetch('/api/hello');
+    const response = await fetch('/api/number');
     const body = await response.json();
 
     if (response.status !== 200) throw Error(body.message);
@@ -32,7 +32,7 @@ class App extends Component {
 
   handleSubmit = async e => {
     e.preventDefault();
-    const response = await fetch('/api/world', {
+    const response = await fetch('/api/number', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
